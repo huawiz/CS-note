3ed6777d31a6b04f90bb9101e2394cd8ca1f7af4
Justin Wu <huawiz@icloud.com>
2022-03-24 23:39:07 +0800

    Update

---
+++ CS50x/pset2/caesar/caesar.c
@@ -0,0 +1,71 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+bool check_digits(string s);
+void caesar(string key);
+char rotate(char letter, int key);
+int main(int argc, string argv[])
+{
+
+    //check program with one command-line arguments and it's digit
+    if (argc == 2 && check_digits(argv[1]))
+    {
+        caesar(argv[1]);
+        return 0;
+    }
+    else
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+}
+
+bool check_digits(string s)
+{
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (isdigit(s[i]) == false)
+        {
+            return 0;
+            break;
+        }
+    }
+    return 1;
+}
+/*
+原始符號+KEY-Z符號+(A符號-1)=加密後的符號
+例如O--+13-->B: 79+13-90+64 = 66
+C_i = ((p_i-64)+k)%26+64 is upper
+C_i = ((p_i-97)+k)%26+97 is lower
+*/
+
+
+void caesar(string key)
+{
+    int num = atoi(key);
+    string text = get_string("plaintext: ");
+    for (int i = 0; i < strlen(text); i++)
+    {
+        text[i] = rotate(text[i], num);
+    }
+    printf("ciphertext: %s\n", text);
+}
+
+char rotate(char letter, int key)
+{
+    if (isupper(letter))
+    {
+        return (((int)letter - 64) + key) % 26 + 64;
+    }
+    else if (islower(letter))
+    {
+        return (((int)letter - 97) + key) % 26 + 97;
+    }
+    else
+    {
+        return letter;
+    }
+}
---
+++ CS50x/pset2/substitution/substitution.c
@@ -0,0 +1,102 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+bool check_repeat(string s);
+bool check_alpha(string s);
+void cipher(string key);
+char rotate(char letter, string key);
+
+int main(int argc, string argv[])
+{
+    //check program with one command-line arguments
+    if (argc == 2)
+    {
+        //check program contain 26 letter.
+        if (strlen(argv[1]) == 26)
+        {
+            //check the key doesn't contain repeat and non-alpha letter.
+            if (check_repeat(argv[1]) && check_alpha(argv[1]))
+            {
+                cipher(argv[1]);
+                return 0;
+            }
+            else if (check_repeat(argv[1]) == false)
+            {
+                printf("Key must not contain repeated characters.\n");
+                return 1;
+            }
+            else
+            {
+                printf("Key must only contain alphabetic characters.\n");
+                return 1;
+            }
+        }
+        else
+        {
+            printf("Key must contain 26 characters.\n");
+            return 1;
+        }
+    }
+    else
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+}
+
+//check repeat
+bool check_repeat(string s)
+{
+    for (int i = 0 ; i < strlen(s); i++)
+    {
+        for (int j = i + 1; j < strlen(s); j++)
+        {
+            if (s[i] == s[j])
+            {
+                return 0;
+                break;
+            }
+        }
+    }
+    return 1;
+}
+
+bool check_alpha(string s)
+{
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (isalpha(s[i]) == false)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void cipher(string key)
+{
+    string text = get_string("plaintext: ");
+    for (int i = 0; i < strlen(text); i++)
+    {
+        text[i] = rotate(text[i], key);
+    }
+    printf("ciphertext: %s\n", text);
+}
+
+char rotate(char letter, string key)
+{
+    if (isupper(letter))
+    {
+        return toupper(key[((int)letter) - 65]);
+    }
+    else if (islower(letter))
+    {
+        return tolower(key[((int)letter) - 97]);
+    }
+    else
+    {
+        return letter;
+    }
+}
